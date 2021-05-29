1e1fdb981b3297edbb9f9b495e36a3b19284f0ed
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-29 15:23:41 +0200

    Made calculator accept modes

--- CalcEngineHex.java
+++ CalcEngineHex.java
@@ -1,17 +1,17 @@
 /**
  * The main part of the calculator doing the calculations.
  * 
- * @author Alex and n-c0de-r
+ * @author AlexJger and n-c0de-r
  * @version 27.05.2021
  */
 public class CalcEngineHex extends CalcEngine {
 		
 	@Override
-    public void numberPressed(int number)
+    public void numberPressed(int number, int mode)
     {
         if(buildingDisplayValue) {
             // Incorporate this digit.
-            displayValue = displayValue*16 + number;
+            displayValue = displayValue*mode + number;
         }
         else {
             // Start building a new number.
